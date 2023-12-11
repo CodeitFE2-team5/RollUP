4ef3a417eba5aa72f9a3ac684dbc4a526c39831a
BBoBBoM <tkrhkakt12@gmail.com>
2023-12-11 17:47:42 +0900

    리스트 페이지 기본화면 구현 carousel 화면 넘기기 작업

--- Rolling/src/App.jsx
+++ Rolling/src/App.jsx
@@ -1,41 +1,14 @@
-import { useEffect } from 'react';
-import { ListPage } from './ListPage';
-
+import { ListPage } from './page/ListPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  // useEffect(() => {
-  //   const emojis = ['👍', '🤣', '😍', '😂', '😊', '😁', '❤️', '👌'];
-
-  //   const numRequests = Math.floor(Math.random() * (30 - 1)) + 10;
-
-  //   for (let i = 0; i < numRequests; i++) {
-  //     const emojiData = {
-  //       emoji: emojis[i % emojis.length],
-  //       type: 'increase',
-  //     };
-
-  //     fetch('https://rolling-api.vercel.app/2-5/recipients/846/reactions/', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(emojiData),
-  //     })
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error(`HTTP 오류! 상태: ${response.status}`);
-  //         }
-  //         console.log(`POST 요청 성공 - ${i + 1}`);
-  //       })
-  //       .catch((error) => {
-  //         console.error('오류:', error);
-  //       });
-  //   }
-  // }, []); // useEffect는 컴포넌트 마운트 시 한 번만 실행
-
   return (
-    <>
-      <ListPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<ListPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Rolling/tailwind.config.js
+++ Rolling/tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-      screens: {
-          sm: '480px',
-          md: '768px',
-          lg: '1025px',
-          xl: '1280px',
-          xl2: '1360px',
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1025px',
+      xl: '1280px',
+      xl2: '1360px',
+    },
+    extend: {
+      backgroundColor: {
+        blue: '#B1E4FF',
+        green: '#D0F5C3',
+        purple: '#ECD9FF',
+        beige: '#FFE2AD',
       },
-      extend: {},
+    },
   },
   plugins: [],
-}
-
+};
