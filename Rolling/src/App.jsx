566473a7b1dfedff98018cdd1190a14771fd0a71
BBoBBoM <tkrhkakt12@gmail.com>
2023-12-12 12:51:44 +0900

    post페이지 기본구현

--- Rolling/src/App.jsx
+++ Rolling/src/App.jsx
@@ -1,9 +1,19 @@
+import { ListPage } from './page/ListPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PostPage } from './page/PostPage';
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/list">
+          <Route index element={<ListPage />} />
+        </Route>
+        <Route path="/post">
+          <Route index element={<PostPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
