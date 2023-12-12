4b42d88bd2c5f02ddcbadf740867eabe583d7654
Ohreum <dhfma3394@naver.com>
2023-12-12 18:03:24 +0900

    fix: pages 폴더 삭제

--- Rolling/src/components/MessagePage/MessageEditPage.jsx
+++ Rolling/src/components/MessagePage/MessageEditPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Rolling/src/pages/MessageEditPage.jsx
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
@@ -32,38 +31,3 @@ function MessageEditPage() {
 }
 
 export default MessageEditPage;
-=======
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-
-function MessageEditPage() {
-  const [messages, setMessages] = useState([]);
-
-  const getRollingData = async () => {
-    try {
-      const response = await axios.get(
-        'https://rolling-api.vercel.app/2-5/recipients/836/messages/'
-      );
-      const { results } = await response.data;
-      setMessages(results);
-    } catch (error) {
-      alert(error);
-    }
-  };
-  useEffect(() => {
-    getRollingData();
-  }, []);
-
-  return (
-    <>
-      <div className="post-body">
-        <button>삭제하기</button>
-        <MessageCardContents messages={messages} showTrashIcon={true} />
-        <MessageCardModal />
-      </div>
-    </>
-  );
-}
-
-export default MessageEditPage;
->>>>>>> upstream/Rolling-v1:Rolling/src/components/MessagePage/MessageEditPage.jsx
