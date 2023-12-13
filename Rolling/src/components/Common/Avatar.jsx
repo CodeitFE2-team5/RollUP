2ce4fa03c20d8b90f3b5c6afe1c0c5f11c98fee1
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-13 17:00:40 +0900

    <refactor>: profileImage 컴포넌트 생성 및 Avatar 컴포넌트에 추가

--- Rolling/src/components/Common/Avatar.jsx
+++ Rolling/src/components/Common/Avatar.jsx
@@ -11,45 +11,30 @@ const Avatar = ({ profiles }) => {
   const renderAvatars = () => {
     if (profiles.length <= MAX_DISPLAY) {
       return profiles.map((profile, index) => (
-        <div key={index} className={`avatar avatar-${index} w-7 rounded-full absolute ${leftValue[index]} left`}>
-          <img src={profile} alt="프로필" className=" rounded-full border"/>
-        </div>
+        <ProfileImage key={index} index={index} profileImage={profile}/>
       ));
     } else {
       const overflowCount = profiles.length - MAX_DISPLAY;
       return (
         <>
           {profiles.slice(0, MAX_DISPLAY).map((profile, index) => (
-            <div key={index} className={`avatar avatar-${index} w-7 rounded-full absolute ${leftValue[index]}`}>
-              <img src={profile} alt="프로필" className=" rounded-full border border-white"/>
-            </div>
+            <ProfileImage key={index} index={index} profileImage={profile}/>
           ))}
           <div className={"avatar avatar-more w-7 h-7 rounded-full border border-gray-200 flex items-center justify-center text-xs bg-white absolute left-12"}>{`+${overflowCount}`}</div>
         </>
       );
     }
   };
-  //   if (profiles.length <= MAX_DISPLAY) {
-  //     return profiles.map((profile, index) => (
-  //       <div key={index} className={`avatar avatar-${index} w-7 rounded-full absolute ${leftValue[index]}`}>
-  //         <img src={profile} alt="프로필" className=" rounded-full border"/>
-  //       </div>
-  //     ));
-  //   } else {
-  //     const overflowCount = profiles.length - MAX_DISPLAY;
-  //     return (
-  //       <>
-  //         {profiles.slice(0, MAX_DISPLAY).map((profile, index) => (
-  //           <div key={index} className={`avatar avatar-${index} w-7 rounded-full absolute ${leftValue[index]}`}>
-  //           <img src={profile} alt="프로필" className=" rounded-full border border-white"/>
-  //           </div>
-  //         ))}
-  //         <div className={"avatar avatar-more w-7 rounded-full border border-gray-200 flex items-center justify-center text-xs absolute h-7 left-12 z-40 bg-white"}>{`+${overflowCount}`}</div>
-  //       </>
-  //     );
-  //   }
-  // };
-
   return <div className="avatar-container flex relative w-20 h-7">{renderAvatars()}</div>;
 };
+
+const ProfileImage = ({ index, profileImage }) => {
+  return(
+    <div key={index} className={`avatar avatar-${index} w-7 rounded-full absolute ${leftValue[index]} left`}>
+      <img src={profileImage} alt="프로필" className=" rounded-full border"/>
+    </div>
+  )
+};
+
+
 export default Avatar;
