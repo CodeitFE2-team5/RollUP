9b00826bedb4760c086e22be2db948324c760750
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-15 01:44:11 +0900

    Fix: 전체 반응을 보여주는 AdditionalReactionContainer가 다른 컴포넌트 아래에 배치되는 문제 해결

--- Rolling/src/components/RecipientMenu/AdditionalReactionContainer.jsx
+++ Rolling/src/components/RecipientMenu/AdditionalReactionContainer.jsx
@@ -2,7 +2,7 @@ import ReactionBadge from "../Common/ReactionBadge";
 
 const AdditionalReactionContainer = () => {
   return(
-    <div className="grid grid-rows-2 grid-flow-col border py-6 px-6 absolute top-11 gap-x-2 gap-y-2.5 border-gray-400 rounded-lg right-24">
+    <div className="grid grid-rows-2 grid-flow-col border py-6 px-6 absolute top-11 gap-x-2 gap-y-2.5 border-gray-400 rounded-lg right-24 z-10 bg-white">
       <ReactionBadge></ReactionBadge>
       <ReactionBadge></ReactionBadge>
       <ReactionBadge></ReactionBadge>
