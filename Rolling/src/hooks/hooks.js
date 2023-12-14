28bd17d7ae17e05d7e11a8b233e0510a4c1ad790
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-14 15:59:13 +0900

    Feat: 롤링페이퍼 데이터를 받아오는 커스텀 훅을 sort를 parameter로 받아서 최신순/인기순으로 받아올 수 있도록 수정하고 useCallback을 사용해서 useEffect 의존성 배열 문제 해결

--- Rolling/src/hooks/hooks.js
+++ Rolling/src/hooks/hooks.js
@@ -1,35 +1,26 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-export const useRollingPaperList = () => {
+const RECIPIENT_API = 'https://rolling-api.vercel.app/2-5/recipients';
+
+export const useRollingPaperList = (sort = '') => {
   const [rollingPaperList, setRollingPaperList] = useState();
+  const [error, setError] = useState();
 
-  const getRollingPaperList = async() => {
+  const getRollingPaperList = useCallback(async() => {
     try{
-      const response = await axios.get('https://rolling-api.vercel.app/2-5/recipients/');
+      const response = await axios.get(`${RECIPIENT_API}/?limit=8&offset=8&sort=${sort}`);
       const { results } = await response.data;
       setRollingPaperList(results);
     } catch(error) {
       alert(error);
+      setError(error);
     }
-  };
+  }, [sort]);
 
   useEffect(() => {
     getRollingPaperList();
-  }, [])
+  }, [getRollingPaperList])
 
-  return rollingPaperList;
+  return [rollingPaperList, error];
 };
-
-// const getRollingData = async() => {
-//   try {
-//     const response = await axios.get('https://rolling-api.vercel.app/2-5/recipients/');
-//     const { results } = await response.data;
-//     console.log(results);
-//   } catch(error) {
-//     alert(error);
-//   }
-// };
-// useEffect(() => {
-//   getRollingData();
-// }, []);
