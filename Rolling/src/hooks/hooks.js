52c3f998ff201c70f1386e9c9ea7e58cdb3fb5e4
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-15 01:25:11 +0900

    Feat: 롤링페이퍼리스트 페이지에서 인기순/최신순 정렬 추가
    롤링페이퍼리스트 페이지 인기순/최신순 정렬 캐러셀 추가
    롤링페이퍼리스트 데이터를 받아오는 커스텀 훅 수정

--- Rolling/src/hooks/hooks.js
+++ Rolling/src/hooks/hooks.js
@@ -1,22 +1,25 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 
-const RECIPIENT_API = 'https://rolling-api.vercel.app/2-5/recipients';
+const LIMIT = 10;
+const RECIPIENT_API = `https://rolling-api.vercel.app/2-5/recipients/?limit=${LIMIT}&offset=0`;
 
 export const useRollingPaperList = (sort = '') => {
-  const [rollingPaperList, setRollingPaperList] = useState();
+  const [rollingPaperList, setRollingPaperList] = useState([]);
+  const [url, setUrl] = useState(`${RECIPIENT_API}&sort=${sort}`);
   const [error, setError] = useState();
 
   const getRollingPaperList = useCallback(async() => {
     try{
-      const response = await axios.get(`${RECIPIENT_API}/?limit=8&offset=8&sort=${sort}`);
-      const { results } = await response.data;
-      setRollingPaperList(results);
+      const response = await axios.get(url);
+      const { next, results } = await response.data;
+      if(next) setUrl(next);
+      setRollingPaperList((prev) => [...prev, ...results]);
     } catch(error) {
       alert(error);
       setError(error);
     }
-  }, [sort]);
+  }, [url]);
 
   useEffect(() => {
     getRollingPaperList();
