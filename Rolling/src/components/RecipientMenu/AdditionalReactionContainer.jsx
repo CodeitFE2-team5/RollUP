7bea24692b118ab90ad55f72d8b416afc3a53bf7
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-16 00:27:48 +0900

    Fix: 반응을 보여주는 AdditionalReactionContainer에서 요구사항에 맞게 반응이 많은 순서대로 8개만 보여주도록 수정

--- Rolling/src/components/RecipientMenu/AdditionalReactionContainer.jsx
+++ Rolling/src/components/RecipientMenu/AdditionalReactionContainer.jsx
@@ -27,7 +27,7 @@ const AdditionalReactionContainer = ({ recipientId }) => {
 
   return(
     <div className="grid grid-rows-2 grid-flow-col border py-6 px-6 absolute top-11 gap-x-2 gap-y-2.5 border-gray-400 rounded-lg right-24 z-10 bg-white">
-      {reactions?.map((reaction) => <ReactionBadge key={reaction.id+reaction.emoji} reaction={reaction} />)}
+      {reactions?.slice(0,8).map((reaction) => <ReactionBadge key={reaction.id+reaction.emoji} reaction={reaction} />)}
     </div>
   )
 };
