a89be0a2ce7f8779b266afffada04b2e257d1f25
Ohreum <dhfma3394@naver.com>
2023-12-16 23:44:34 +0900

    Feat: 해당하는 id의 메세지 불러오도록 api 주소 변경, 메세지 페이지에 편집하기 버튼 생성 및 경로 설정

--- Rolling/src/components/MessagePage/MessageCardContents.jsx
+++ Rolling/src/components/MessagePage/MessageCardContents.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import MessageCard from './MessageCard';
 import MessageCardModal from './MessageCardModal.jsx';
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import { BACKGROUND_COLOR } from '../../constants/constants.js';
-import { useState } from 'react';
+import MessageLoading from '../MessagePage/MessageLoading';
+import { Link } from 'react-router-dom';
 
-function MessageCardContents({ recipient, messages }) {
+function MessageCardContents({ recipient, messages, postId, loading }) {
   const [clickedMessage, setClickedMessage] = useState([]);
   const [openModal, setOpenModal] = useState(false);
 
@@ -26,44 +28,56 @@ function MessageCardContents({ recipient, messages }) {
   };
 
   return (
-    <div
-      className={`w-screen min-h-screen ${
-        BACKGROUND_COLOR[recipient?.backgroundColor]
-      } pt-28 pb-60 pl-6 pr-[34px]`}
-    >
-      <div className="lg:grid lg:grid-cols-[repeat(3,minmax(0,24rem))] md:grid md:grid-cols-[repeat(2,minmax(0,24rem))] grid grid-cols-[repeat(1,minmax(0,24rem))] justify-center gap-4">
-        <div className="flex justify-center items-center max-w-sm h-[280px] shadow-[0px_2px_12px_0px_rgba(0,0,0,0.08)] rounded-2xl bg-white">
-          <button className="flex items-start gap-2.5 p-4">
-            <BsFillPlusCircleFill className="w-14 h-14 fill-gray-500 grid-cols" />
+    <>
+      <div
+        className={`flex flex-col items-end gap-3.5 w-screen min-h-screen ${
+          BACKGROUND_COLOR[recipient?.backgroundColor]
+        } pt-28 pb-60 pl-6 pr-[34px]`}
+      >
+        <Link to={`/post/${postId}/edit`}>
+          <button className="hover:bg-purple-700 w-32 px-6 py-3.5 bg-purple-600 rounded-xl justify-center items-center gap-2.5 inline-flex text-center text-white text-lg font-bold font-['Pretendard'] leading-7">
+            편집하기
           </button>
+        </Link>
+
+        <div className="lg:grid lg:grid-cols-[repeat(3,minmax(0,24rem))] md:grid md:grid-cols-[repeat(2,minmax(0,24rem))] grid grid-cols-[repeat(1,minmax(0,24rem))] justify-center gap-4">
+          <div className="flex justify-center items-center max-w-sm h-[280px] shadow-[0px_2px_12px_0px_rgba(0,0,0,0.08)] rounded-2xl bg-white">
+            <button className="flex items-start gap-2.5 p-4">
+              <BsFillPlusCircleFill className="w-14 h-14 fill-gray-500 grid-cols" />
+            </button>
+          </div>
+
+          {messages?.map((message) => (
+            <MessageCard
+              key={message.id}
+              message={message}
+              onClickMessage={onClickMessage}
+              showTrashIcon={false}
+            />
+          )) ?? null}
+
+          {openModal && (
+            <>
+              <MessageCardModal message={clickedMessage} handleCloseModal={handleCloseModal} />
+              <div
+                onClick={handleCloseModal}
+                className="fixed bg-[black] opacity-[60%] inset-0"
+              ></div>
+            </>
+          )}
         </div>
-
-        {messages?.map((message) => (
-          <MessageCard
-            key={message.id}
-            message={message}
-            onClickMessage={onClickMessage}
-            showTrashIcon={false}
-          />
-        )) ?? null}
-
-        {openModal && (
-          <>
-            <MessageCardModal message={clickedMessage} handleCloseModal={handleCloseModal} />
-            <div
-              onClick={handleCloseModal}
-              className="fixed bg-[black] opacity-[60%] inset-0"
-            ></div>
-          </>
-        )}
       </div>
-    </div>
+
+      {loading && <MessageLoading />}
+    </>
   );
 }
 
 MessageCardContents.propTypes = {
   recipient: PropTypes.object,
   messages: PropTypes.array,
+  loading: PropTypes.bool,
+  postId: PropTypes.number,
 };
 
 export default MessageCardContents;
