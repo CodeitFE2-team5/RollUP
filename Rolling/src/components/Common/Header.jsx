1c241776cdeacbd7358c707793e162f3813ce8f2
imkrmin <zum2n2@gmail.com>
2023-12-17 14:52:02 +0900

    Style: Header 컴포넌트 미디어 쿼리 적용

--- Rolling/src/components/Common/Header.jsx
+++ Rolling/src/components/Common/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
   const isVisiblePage = location.pathname === '/' || location.pathname === '/list';
 
   return (
-    <div className="flex items-center justify-between flex-shrink-0 py-2 md:px-24 max-w-screen-xl mx-auto h-16 border-b-[1px] border-zinc-100">
-      <Link to={'/'}>
+    <div className="flex items-center justify-between flex-shrink-0 py-2 px-4 xl:px-24 max-w-screen-xl mx-auto h-16 border-b-[1px] border-zinc-100">
+      <Link to={'/'} className="flex-shrink-0">
         <img src={logo} alt="로고" />
       </Link>
       {isVisiblePage && (
-        <Link to={'/post'}>
+        <Link to={'/post'} className="ml-auto">
           <button className="hover:bg-neutral-100 w-40 px-4 py-2 bg-white rounded-md border border-gray-300 justify-center items-center gap-2.5 flex text-center text-neutral-900 text-base font-bold font-pre">
             롤링 페이퍼 만들기
           </button>
