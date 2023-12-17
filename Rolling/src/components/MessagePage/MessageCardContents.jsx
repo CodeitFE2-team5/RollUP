1e8f51781aa7dd2aa19f55a14c9132608d790aa5
Ohreum <dhfma3394@naver.com>
2023-12-18 00:40:27 +0900

    Feat: 편집 모드 시에 '+'버튼이 있는 카드 안보이게 구현

--- Rolling/src/components/MessagePage/MessageCardContents.jsx
+++ Rolling/src/components/MessagePage/MessageCardContents.jsx
@@ -61,11 +61,13 @@ function MessageCardContents({ recipient, messages, postId, loading }) {
         )}
 
         <div className="lg:grid lg:grid-cols-[repeat(3,minmax(0,24rem))] md:grid md:grid-cols-[repeat(2,minmax(0,24rem))] grid grid-cols-[repeat(1,minmax(0,24rem))] justify-center gap-4">
-          <div className="flex justify-center items-center max-w-sm h-[280px] shadow-[0px_2px_12px_0px_rgba(0,0,0,0.08)] rounded-2xl bg-white">
-            <button className="flex items-start gap-2.5 p-4">
-              <BsFillPlusCircleFill className="w-14 h-14 fill-gray-500 grid-cols" />
-            </button>
-          </div>
+          {!location.pathname.includes('edit') && (
+            <div className="flex justify-center items-center max-w-sm h-[280px] shadow-[0px_2px_12px_0px_rgba(0,0,0,0.08)] rounded-2xl bg-white">
+              <button className="flex items-start gap-2.5 p-4">
+                <BsFillPlusCircleFill className="w-14 h-14 fill-gray-500 grid-cols" />
+              </button>
+            </div>
+          )}
 
           {!location.pathname.includes('edit')
             ? messages?.map((message) => (
