a6c0a1c46490aa0668eb0ce01c3cf55918dabf45
JaeHwan <131963909+BBoBBoM@users.noreply.github.com>
2023-12-20 17:14:36 +0900

    [김재환] Fix: 모달 UI수정 Input 오류메시지수정 (#37)
    * [김재환] Feat: 파일추가 기능구현
    
    IBB사이트 포스트요청으로 받아온 URL을 화면에 보여주는 방식으로 구현
    
    * [김재환] Fix: 배경이미지 api 수정 반응형 css 수정
    
    Codeit제공 api를 통한 기본제공  background 사용
    mobile에서의 image 버튼부분 css수정
    File로 업로드시 Loading 처리(팀원 의견 필요)
    
    * [김재환] Fix: 모달 UI수정 Input 오류메시지수정
    
    모달 UI수정 Input 오류메시지 input 태그 아래 보이도록 수정
    이미지 추가 부분 +버튼 제일 앞쪽으로 배치 앞에서부터 채워지도록 순서
    수정
    
    ---------
    
    Co-authored-by: VIN <51733830+lhv0829@users.noreply.github.com>

--- Rolling/src/api.js
+++ Rolling/src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+//Codeit에서 제공하는 기본 background이미지
 export async function getBackgroundList() {
   const URL = 'https://rolling-api.vercel.app/background-images/';
   try {
@@ -16,6 +17,7 @@ export async function getBackgroundList() {
   }
 }
 
+//recipients post요청
 export async function createRecipient(formData) {
   const url = 'https://rolling-api.vercel.app/2-5/recipients/';
 
@@ -30,5 +32,33 @@ export async function createRecipient(formData) {
   } catch (error) {
     console.error('Error while fetching data:', error);
     throw error;
+  }
+}
+
+//IBB사이트 포스트 요청기능 이미지 업로드후 업로드한 이미지 url받아오기
+export async function uploadImageIBB(file) {
+  const formData = new FormData();
+  formData.append('image', file);
+
+  try {
+    const response = await fetch(
+      'https://api.imgbb.com/1/upload?expiration=518400&key=95d8f7e8290e8ffef56546465f96b389',
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Uploaded Image URL:', data.data.url);
+      return data.data.url;
+    } else {
+      console.error('업로드실패');
+      return null;
+    }
+  } catch (error) {
+    console.error('업로드실패:', error);
+    return null;
   }
 }
--- Rolling/src/components/PostPage/PostPage.jsx
+++ Rolling/src/components/PostPage/PostPage.jsx
@@ -6,10 +6,11 @@ import Subject from './Subject';
 import { CreateButton } from './CreateButton';
 import { UserNameInput } from './UserNameInput';
 import { ToggleButton } from './ToggleButton';
-import { createRecipient } from '../../api';
+import { createRecipient, getBackgroundList } from '../../api';
 import OptionSelectContainer from './OptionSelectContainer';
 import { UrlModal } from './UrlModal';
 import NoSelectBackgroundCheck from './NoBackgroundCheck';
+import { UrlAppendButton } from './UrlAppendButton';
 
 const colors = [`bg-[#ECD9FF]`, `bg-[#D0F5C3]`, `bg-[#B1E4FF]`, `bg-[#FFE2AD]`];
 const colorMap = {
@@ -26,47 +27,48 @@ const PostPage = () => {
   const [selectedColor, setSelectedColor] = useState(colors[0]);
   const [selectedImage, setSelectedImage] = useState('');
   const [images, setImages] = useState([]);
-  const [receiveUserName, setReceiveUserName] = useState('');
-  const [nameInputEmpty, setNameInputEmpty] = useState(true);
+  const [userName, setUserName] = useState('');
   const [modalOpen, setModalOpen] = useState(false);
   const [isChecked, setChecked] = useState(false);
 
-  const imageUrls = [
-    'https://cdn.pixabay.com/photo/2017/11/07/20/43/christmas-tree-2928142_1280.jpg',
-    'https://cdn.pixabay.com/photo/2018/12/18/21/52/new-years-eve-3883137_1280.png',
-    'https://cdn.pixabay.com/photo/2018/11/09/19/57/christmas-3805334_1280.jpg',
-    'https://cdn.pixabay.com/photo/2012/04/13/01/23/moon-31665_1280.png',
-  ];
-
   const fetchData = async () => {
     try {
+      const { imageUrls } = await getBackgroundList();
+
       setImages(imageUrls);
       if (imageUrls.length > 0) {
         setSelectedImage(imageUrls[0]);
       }
     } catch (error) {
-      console.error(error);
+      console.error('Error fetching background list:', error);
     }
   };
   const handleItemClick = (option, value) => {
     setSelectOption(option);
 
-    option === 'color' ? setSelectedColor(value) : (setSelectedImage(value), setChecked(false));
+    if (option === 'color') {
+      setSelectedColor(value);
+    } else {
+      setSelectedImage(value);
+      setChecked(false);
+    }
   };
   const handleSetImageArray = (value) => {
-    setImages((prev) => [...prev, value]);
+    setImages((prev) => [value, ...prev]);
   };
-  const handleNameChange = (e) => {
-    const inputValue = e.target.value;
-    setReceiveUserName(inputValue);
-    inputValue !== '' ? setNameInputEmpty(true) : setNameInputEmpty(false);
+  const handleNameChange = (value) => {
+    setUserName(value);
   };
   const handleModalChange = (bool, value) => {
     setModalOpen(bool);
 
-    value !== null
-      ? (setImages((prev) => [...prev, value]), setSelectedImage(value), setChecked(false))
-      : setSelectedImage(selectedImage);
+    if (value !== null) {
+      setImages((prev) => [value, ...prev]);
+      setSelectedImage(value);
+      setChecked(false);
+    } else {
+      setSelectedImage(selectedImage);
+    }
   };
 
   const handleCheckboxChange = () => {
@@ -74,48 +76,54 @@ const PostPage = () => {
     setSelectedImage('');
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (selectedColor === '') {
-      return alert('색상은 필수선택 사항입니다.');
-    }
-    const formData = new FormData();
-    formData.append('team', '2-5');
-    formData.append('name', receiveUserName);
-
-    formData.append('backgroundColor', colorMap[selectedColor]);
-    {
-      selectedImage && formData.append('backgroundImageURL', selectedImage);
-    }
+  const handleResponse = async (responseData) => {
     try {
-      const responseData = await createRecipient(formData);
-
       if (responseData.id) {
         navigate(`/post/${responseData.id}`);
       } else {
-        console.error('Invalid id in responseData:', responseData);
+        console.error('responseData에 유효한 id가 없습니다:', responseData);
       }
     } catch (error) {
-      console.error('Error creating recipient:', error);
+      console.error('응답 처리 중 오류 발생:', error);
     }
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (selectedColor === '') {
+      return alert('색상은 필수 선택 항목입니다.');
+    }
+
+    const formData = new FormData();
+
+    formData.append('name', userName);
+    formData.append('backgroundColor', colorMap[selectedColor]);
+
+    if (selectedImage) {
+      formData.append('backgroundImageURL', selectedImage);
+    }
+
+    try {
+      const responseData = await createRecipient(formData);
+      handleResponse(responseData);
+    } catch (error) {
+      console.error('수령인 생성 중 오류 발생:', error);
+    }
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
-    <div className="m-full px-5 ">
+    <div className="m-full px-5 overflow-hidden">
       <form
         onSubmit={handleSubmit}
         className="w-[320px] mx-auto mt-[57px] flex flex-col box-border sm:w-[720px] "
       >
-        <div className="flex flex-col  gap-3 mb-[50px] ">
+        <div className="flex flex-col  gap-3 mb-[34px] ">
           <Subject subject="To." description="" />
-          <UserNameInput
-            value={receiveUserName}
-            onChange={handleNameChange}
-            nameInputEmpty={nameInputEmpty}
-          />
+          <UserNameInput onChange={handleNameChange} />
         </div>
 
         <Subject
@@ -123,7 +131,7 @@ const PostPage = () => {
           description="컬러를 선택하거나, 이미지를 선택할 수 있습니다"
         />
         <div className="flex justify-between">
-          <div className="w-[220px] flex  bg-gray-200 text-center mb-11  text-lg font-Pretendard font-bold rounded-md  ">
+          <div className="w-[240px] flex  bg-gray-200 text-center mb-11  text-lg font-Pretendard font-bold rounded-md  ">
             <ToggleButton
               onClick={() => handleItemClick('color', selectedColor)}
               isActive={selectOption === 'color'}
@@ -138,22 +146,17 @@ const PostPage = () => {
           </div>
 
           {selectOption === 'image' && (
-            <div className="flex gap-1 w-[270px]   text-center mb-11  text-lg font-Pretendard font-bold rounded-md ">
+            <div className="flex flex-col gap-1 w-[210px]  text-center mb-11  text-lg font-Pretendard font-bold rounded-md sm:flex-row sm:w-[260px]">
               <NoSelectBackgroundCheck
                 handleCheckboxChange={handleCheckboxChange}
                 isChecked={isChecked}
               />
-              <div
-                className=" p-[7px] px-[16px] border-2 border-gray-200 cursor-pointer rounded-md transition-transform transform hover:scale-110"
-                onClick={() => setModalOpen(true)}
-              >
-                URL추가
-              </div>
+              <UrlAppendButton onclick={() => setModalOpen(true)} />
             </div>
           )}
         </div>
 
-        {selectOption === 'color' ? (
+        {images.length > 0 && selectOption === 'color' ? (
           <OptionSelectContainer
             optionArray={colors}
             selectedList={selectedColor}
@@ -170,9 +173,7 @@ const PostPage = () => {
             handleSetImageArray={handleSetImageArray}
           />
         )}
-        <CreateButton onSubmit={handleSubmit} disabled={!receiveUserName}>
-          생성하기
-        </CreateButton>
+        <CreateButton onSubmit={handleSubmit} disabled={!userName} />
       </form>
       <div>{modalOpen && <UrlModal handleModalChange={handleModalChange} />}</div>
     </div>
