e63cbe4748e4997f95c1bc1a7a51096d24634a21
Ohreum Son <105328855+mrhandsup@users.noreply.github.com>
2023-12-21 13:58:27 +0900

    [손오름] Refactor: 컴포넌트 분리 및 api 호출부 분리 작업 (#46)
    * Feat: 편집모드에서 삭제할 카드를 하나도 선택하지 않았을 시 저장하기 버튼 비활성화 되게 구현
    
    * Refactor(MessageCardContents): 메세지 페이지에 사용되는 버튼들 컴포넌트로 분리
    
    * Refactor: 메세지 카드 렌더링 하는 로직 컴포넌트로 분리
    
    - 메세지 모달도 같은 컴포넌트에 분리
    - MessageCardContents 이름 변경
    
    * Feat: 로딩 시에 메세지 카드 렌더링 되는 부분 스켈레톤 처리
    
    * Refactor: api 호출 부분 따로 분리
    
    * Fix: 태블릿 이하 사이즈에서 저장하기 버튼 z-index 이슈 수정

---
+++ src/components/MessagePage/MessageCardBody.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { BACKGROUND_COLOR } from '../../constants/constants.js';
+import MessageButtons from './MessageButtons.jsx';
+import MessageCardLists from './MessageCardLists.jsx';
+
+function MessageCardBody({ recipient, messages, postId, initialSkeletonLoading }) {
+  const [clickedMessageIds, setClickedMessageIds] = useState([]);
+
+  const getClickedMessageIds = (clickedMessageId) => {
+    setClickedMessageIds((prevSet) => {
+      const updatedSet = new Set(prevSet);
+      if (updatedSet.has(clickedMessageId)) {
+        updatedSet.delete(clickedMessageId);
+      } else {
+        updatedSet.add(clickedMessageId);
+      }
+
+      const updatedArray = Array.from(updatedSet);
+      return updatedArray;
+    });
+  };
+
+  return (
+    <>
+      <div
+        style={{ backgroundImage: `url(${recipient?.backgroundImageURL})` }}
+        className={`flex flex-wrap content-center flex-col items-end gap-3.5 w-full min-h-screen pt-16 pb-60 pl-6 pr-[34px] bg-cover 
+        ${BACKGROUND_COLOR[recipient?.backgroundColor]}`}
+      >
+        <MessageButtons
+          postId={postId}
+          recipient={recipient}
+          disabled={clickedMessageIds.length}
+          clickedMessageIds={clickedMessageIds}
+          setClickedMessageIds={setClickedMessageIds}
+        />
+        <MessageCardLists
+          postId={postId}
+          messages={messages}
+          getClickedMessageIds={getClickedMessageIds}
+          initialSkeletonLoading={initialSkeletonLoading}
+        />
+      </div>
+    </>
+  );
+}
+
+MessageCardBody.propTypes = {
+  recipient: PropTypes.object,
+  messages: PropTypes.array,
+  initialSkeletonLoading: PropTypes.bool,
+  postId: PropTypes.string,
+};
+
+export default MessageCardBody;
