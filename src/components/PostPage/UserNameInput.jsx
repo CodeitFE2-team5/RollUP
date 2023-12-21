815c28e5c6e3a17f1738d855d48d14197eee0084
BBoBBoM <tkrhkakt12@gmail.com>
2023-12-21 20:59:01 +0900

    [김재환] Fix: input 입력에 글자수 제한 요구수정
    input입력시 글자수 제한 40자 제한 수정

--- src/components/PostPage/UserNameInput.jsx
+++ src/components/PostPage/UserNameInput.jsx
@@ -6,15 +6,16 @@ const propTypes = {
 };
 
 export const UserNameInput = ({ onChange }) => {
+  const [inputValue, setInputValue] = useState('');
   const [isInputEmpty, setIsInputEmpty] = useState(false);
 
   const handleChange = (e) => {
-    const inputValue = e.target.value;
-    if (inputValue !== '') {
-      setIsInputEmpty(false);
-      onChange(inputValue);
-    } else {
-      setIsInputEmpty(true);
+    const value = e.target.value;
+
+    if (value.length <= 40) {
+      setInputValue(value);
+      setIsInputEmpty(value === '');
+      onChange(value);
     }
   };
 
@@ -23,18 +24,20 @@ export const UserNameInput = ({ onChange }) => {
       <input
         type="text"
         name="receiveUserName"
+        value={inputValue}
         onChange={handleChange}
         placeholder="받는 사람 이름을 입력해 주세요"
-        className={`w-full mb-1 p-4 border-2 border-solid rounded-lg focus:border-2 focus:outline-none  ${
-          isInputEmpty === false
-            ? 'border-gray-300 focus:border-purple-500'
-            : 'border-red-500 focus:border-red-500'
+        maxLength={40}
+        className={`w-full mb-1 p-4 border-2 border-solid rounded-lg focus:border-2 focus:outline-none ${
+          isInputEmpty || inputValue.length >= 40
+            ? 'border-red-500 focus:border-red-500'
+            : 'border-gray-300 focus:border-purple-500'
         }`}
       />
-      {isInputEmpty === true ? (
-        <div className="text-red-500 text-xs">필수입력 사항입니다.</div>
-      ) : (
-        <div className="h-4"></div>
+      <div className={`h-4 ${isInputEmpty || inputValue.length >= 40 ? 'hidden' : ''}`}>&nbsp;</div>
+      {isInputEmpty && <div className="text-red-500 text-xs">필수입력 사항입니다.</div>}
+      {inputValue.length >= 40 && (
+        <div className="text-red-500 text-xs">최대 40자까지 입력 가능합니다.</div>
       )}
     </div>
   );
