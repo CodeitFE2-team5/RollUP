69395f13b83791b45b9b1fa5b9d9790f033876a7
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-21 22:41:35 +0900

    Fix: 공유할 때 pathname만 나오던 url을 전체가 나오도록 수정

--- src/components/RecipientMenu/ShareMenu.jsx
+++ src/components/RecipientMenu/ShareMenu.jsx
@@ -6,7 +6,7 @@ const { Kakao } = window;
 
 const ShareMenu = ({onChange}) => {
   const [isShareSuccess, setIsShareSuccess] = useState(false);
-  const location = useLocation();
+  const url = window.location.href;
 
   const handleShareUrl = async (text) => {
     try {
@@ -31,16 +31,16 @@ const ShareMenu = ({onChange}) => {
         description: '나만의 롤링페이퍼',
         imageUrl:'https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg',
         link: {
-          mobileWebUrl: location.pathname,
-          webUrl: location.pathname,
+          mobileWebUrl: url,
+          webUrl: url,
         },
       },
       buttons: [
         {
           title: '보러가기',
           link: {
-            mobileWebUrl: location.pathname,
-            webUrl: location.pathname,
+            mobileWebUrl: url,
+            webUrl: url,
           },
         },
       ],
@@ -52,15 +52,11 @@ const ShareMenu = ({onChange}) => {
     Kakao.init('dbe2d5520b7b431c84cd34e00872312c');
   },[])
 
-  useEffect(() => {
-    console.log(location.pathname);
-  },[location.pathname]);
-
   return(
     <>
       <div className="flex flex-col py-2.5 px-px border border-gray-300 rounded-lg w-[140px] absolute top-11 right-0 z-10 bg-white">
         <button className="py-3 px-4 hover:bg-gray-100" onClick={() => shareKakao()}>카카오톡 공유</button>
-        <button className="py-3 px-4 hover:bg-gray-100" onClick={() => handleShareUrl(location.pathname)}>URL 공유</button>
+        <button className="py-3 px-4 hover:bg-gray-100" onClick={() => handleShareUrl(url)}>URL 공유</button>
       </div>
       {isShareSuccess && <ToastModal />}
     </>
