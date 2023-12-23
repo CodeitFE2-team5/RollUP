957d3c4f2b2617cb5769a99567d9b7dcf2c7a818
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-23 17:54:49 +0900

    Fix: Subject 컴포넌트 경로 수정

--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Subject from './Subject';
+import Subject from '../Common/Subject';
 import PostButton from '../Common/PostButton';
 import ToggleButton from './ToggleButton';
 import OptionSelectContainer from './OptionSelectContainer';
