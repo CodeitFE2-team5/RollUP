963be7236a1937bf161513a5e19a9fb0565c6593
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-23 17:46:22 +0900

    Fix: export default한 컴포넌트 import할 때 중괄호 삭제

--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Subject from './Subject';
 import PostButton from '../Common/PostButton';
-import { ToggleButton } from './ToggleButton';
+import ToggleButton from './ToggleButton';
 import OptionSelectContainer from './OptionSelectContainer';
 import UrlModal from './UrlModal';
 import NoSelectBackgroundCheck from './NoBackgroundCheck';
